329e40b0a2cc56e4c6cd4830f666e20a2f6f93a3
Ruchit Patel <ruchit.patel@viitor.cloud>
2021-08-29 09:57:08 +0530

    Add workshops relationship.

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function workShops()
+    {
+        return $this->hasMany(Workshop::class);
+    }
 }
